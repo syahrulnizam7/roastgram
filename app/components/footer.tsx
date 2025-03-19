83656ede91eb2696a5a7a8aac3d3b480ef7d4ddb
syahrulnizam <syhrulnizm7@gmail.com>
2025-03-20 06:17:04 +0700

    footer

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import { FaGithub } from "react-icons/fa"; // Import ikon GitHub dari react-icons
+import { FaGithub, FaInstagram } from "react-icons/fa"; // Import ikon GitHub dan Instagram dari react-icons
 
 export default function Footer() {
   const [darkMode, setDarkMode] = useState(false);
@@ -26,7 +26,7 @@ export default function Footer() {
   }, [darkMode]);
 
   return (
-    <footer className={`w-full py-6 px-4  flex items-center justify-between`}>
+    <footer className={`w-full py-6 px-4 flex items-center justify-between`}>
       <p
         className={`text-sm font-bold ${
           darkMode ? "text-white" : "text-black"
@@ -35,18 +35,33 @@ export default function Footer() {
         by alangkun
       </p>
 
-      <a
-        href="https://github.com/syahrulnizam7/roastgram"
-        target="_blank"
-        rel="noopener noreferrer"
-        className={`p-2 rounded-md ${
-          darkMode ? "bg-zinc-800" : "bg-white"
-        } border-2 border-black shadow-[2px_2px_0px_0px_rgba(0,0,0,1)] hover:shadow-[3px_3px_0px_0px_rgba(0,0,0,1)] transition-shadow`}
-      >
-        <FaGithub
-          className={`w-5 h-5 ${darkMode ? "text-white" : "text-black"}`}
-        />
-      </a>
+      <div className="flex gap-2">
+        <a
+          href="https://github.com/syahrulnizam7/roastgram"
+          target="_blank"
+          rel="noopener noreferrer"
+          className={`p-2 rounded-md ${
+            darkMode ? "bg-zinc-800" : "bg-white"
+          } border-2 border-black shadow-[2px_2px_0px_0px_rgba(0,0,0,1)] hover:shadow-[3px_3px_0px_0px_rgba(0,0,0,1)] transition-shadow`}
+        >
+          <FaGithub
+            className={`w-5 h-5 ${darkMode ? "text-white" : "text-black"}`}
+          />
+        </a>
+
+        <a
+          href="https://instagram.com/alang.kun"
+          target="_blank"
+          rel="noopener noreferrer"
+          className={`p-2 rounded-md ${
+            darkMode ? "bg-zinc-800" : "bg-white"
+          } border-2 border-black shadow-[2px_2px_0px_0px_rgba(0,0,0,1)] hover:shadow-[3px_3px_0px_0px_rgba(0,0,0,1)] transition-shadow`}
+        >
+          <FaInstagram
+            className={`w-5 h-5 ${darkMode ? "text-white" : "text-black"}`}
+          />
+        </a>
+      </div>
     </footer>
   );
 }
