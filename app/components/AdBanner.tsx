3ab9f684419164cdd2ad7acd390880f8c799e7fe
syahrulnizam <syhrulnizm7@gmail.com>
2025-04-04 17:02:34 +0700

    tes iklan

--- app/components/AdBanner.tsx
+++ app/components/AdBanner.tsx
@@ -2,6 +2,7 @@ import { useEffect } from "react";
 
 export default function AdBanner() {
   useEffect(() => {
+    // Load script iklan
     const script = document.createElement("script");
     script.src = "//101047.shop/js/responsive.js";
     script.async = true;
@@ -13,27 +14,15 @@ export default function AdBanner() {
   }, []);
 
   return (
-    <div className="w-full flex justify-center py-4 bg-white">
-      {" "}
-      {/* Container utama */}
-      <div className="text-center">
-        {" "}
-        {/* Container untuk centering */}
-        <ins
-          style={{
-            width: "100%", // Responsive width
-            height: "250px", // Sesuaikan tinggi
-            display: "block",
-            margin: "0 auto",
-            maxWidth: "970px", // Batas maksimal lebar
-          }}
-          data-width="0"
-          data-height="0"
-          className="na065232fba"
-          data-domain="//101047.shop"
-          data-affquery="/056416dae0209cc31ff7/a065232fba/?placementName=default"
-        ></ins>
-      </div>
+    <div className="ad-container flex justify-center">
+      <ins
+        style={{ width: "300px", height: "250px", display: "block" }}
+        data-width="300"
+        data-height="250"
+        className="d473e55225c"
+        data-domain="//101047.shop"
+        data-affquery="/056416dae0209cc31ff7/473e55225c/?placementName=default"
+      ></ins>
     </div>
   );
 }
