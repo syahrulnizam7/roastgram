82e626913c26afb26da1857fc83c15084b7cad44
syahrulnizam <syhrulnizm7@gmail.com>
2025-04-04 16:45:16 +0700

    tes iklan

--- app/components/AdBanner.tsx
+++ app/components/AdBanner.tsx
@@ -14,26 +14,15 @@ export default function AdBanner() {
   }, []);
 
   return (
-    <div className="ad-container w-full bg-white py-4">
-      {" "}
-      {/* Tambahkan bg-white dan padding */}
-      <div className="max-w-7xl mx-auto px-4">
-        {" "}
-        {/* Container untuk membatasi lebar konten */}
-        <ins
-          style={{
-            width: "100%", // Ubah jadi 100%
-            height: "250px", // Sesuaikan tinggi
-            display: "block",
-            margin: "0 auto", // Pusatkan
-          }}
-          data-width="100%"
-          data-height="250"
-          className="d473e55225c"
-          data-domain="//101047.shop"
-          data-affquery="/056416dae0209cc31ff7/473e55225c/?placementName=default"
-        ></ins>
-      </div>
+    <div className="ad-container">
+      <ins
+        style={{ width: "300px", height: "250px", display: "block" }}
+        data-width="300"
+        data-height="250"
+        className="d473e55225c"
+        data-domain="//101047.shop"
+        data-affquery="/056416dae0209cc31ff7/473e55225c/?placementName=default"
+      ></ins>
     </div>
   );
 }
