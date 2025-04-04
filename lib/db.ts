b09874a12d4f445eed73a121347801cad7673eb9
syahrulnizam <syhrulnizm7@gmail.com>
2025-04-04 13:28:33 +0700

    error vercel fixed 3

--- lib/db.ts
+++ lib/db.ts
@@ -1,9 +1,4 @@
 import mysql from "mysql2/promise";
-import fs from "fs";
-import path from "path";
-
-// Download CA cert dari Aiven Console dan simpan di root project sebagai 'ca.pem'
-const caCert = fs.readFileSync(path.join(process.cwd(), "ca.pem"));
 
 const pool = mysql.createPool({
   host: process.env.AIVEN_MYSQL_HOST,
@@ -11,12 +6,13 @@ const pool = mysql.createPool({
   user: process.env.AIVEN_MYSQL_USER,
   password: process.env.AIVEN_MYSQL_PASSWORD,
   database: process.env.AIVEN_MYSQL_DB_NAME,
-  ssl: {
-    rejectUnauthorized: true,
-    ca: caCert,
-    // Tambahkan opsi berikut untuk development
-    minVersion: "TLSv1.2",
-  },
+  ssl: process.env.AIVEN_CA_CERT
+    ? {
+        rejectUnauthorized: true,
+        ca: Buffer.from(process.env.AIVEN_CA_CERT, "base64"),
+        minVersion: "TLSv1.2",
+      }
+    : undefined,
   waitForConnections: true,
   connectionLimit: 10,
 });
